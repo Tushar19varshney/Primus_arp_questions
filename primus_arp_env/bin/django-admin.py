9f8d49a27ef3ef8e3f4a755ee5aa2838669fa0c0
deepak <durgesh123.iitr@gmail.com>
2018-10-12 16:55:50 +0530

    Environment has been removed

--- primus_arp_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/compbio/durgesh_api/Question_django_rest/primus_arp_env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
